9dcfc86f9e87942f863182ace16345cfb4ffafd5
Fumiya Kume <fumiya.kume@hotmail.com>
2018-07-09 13:23:26 +0900

    メルカリのAPIを追加

---
+++ mercari/index.js
@@ -0,0 +1,9 @@
+const RssParser = require('../lib/RssParser');
+
+module.exports = async function (context, req) {
+    const RssUrl = `https://tech.mercari.com/feed`;
+    const rssParser = new RssParser(RssUrl);
+    const RssContent = await rssParser.Load();
+    context.log(RssContent);
+    context.res = {body:RssContent};
+};
