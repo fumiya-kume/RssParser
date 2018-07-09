619f23e2bfeffc8eb514ddc067cbde5728cd03f3
Fumiya Kume <fumiya.kume@hotmail.com>
2018-07-09 13:26:34 +0900

    PC User のAPI を追加

---
+++ PC_User/index.js
@@ -0,0 +1,9 @@
+const RssParser = require('../lib/RssParser');
+
+module.exports = async function (context, req) {
+    const RssUrl = `https://rss.itmedia.co.jp/rss/1.0/pcupdate.xml`;
+    const rssParser = new RssParser(RssUrl);
+    const RssContent = await rssParser.Load();
+    context.log(RssContent);
+    context.res = {body:RssContent};
+};
