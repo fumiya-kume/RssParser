297bb7476bcd7819dc0c25d6246dff7db00add7e
Fumiya Kume <fumiya.kume@hotmail.com>
2018-07-09 13:23:19 +0900

    Line のAPIを追加

---
+++ Line/index.js
@@ -0,0 +1,9 @@
+const RssParser = require('../lib/RssParser');
+
+module.exports = async function (context, req) {
+    const RssUrl = `https://engineering.linecorp.com/ja/blog/rss2`;
+    const rssParser = new RssParser(RssUrl);
+    const RssContent = await rssParser.Load();
+    context.log(RssContent);
+    context.res = {body:RssContent};
+};
