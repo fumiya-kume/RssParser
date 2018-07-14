5cf694afa17f37938eefab6f4c5a2f990329633a
Fumiya Kume <fumiya.kume@hotmail.com>
2018-07-15 01:39:44 +0900

    サムネイルを取得できるようにした

--- cookpad/index.js
+++ cookpad/index.js
@@ -5,6 +5,12 @@ module.exports = async function (context, req) {
     const rssParser = new RssParser(pixivUrl);
     const RssContent = await rssParser.Load();
     context.log(RssContent);
+    RssContent.map(item =>{
+        if(!item.Thumbnail){
+            item.Thumbnail = "https://axxis.co.jp/magazine/wp-content/uploads/2016/01/cookpad.jpg";
+        }
+        return item
+    })
     context.bindings.RssBlob = RssContent
     context.res = {body:RssContent};
 };
