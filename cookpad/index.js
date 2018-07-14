c4214769332276264fd92a35d180b7c4f9a55fa5
Fumiya Kume <fumiya.kume@hotmail.com>
2018-07-15 01:42:52 +0900

    サムネイルがnull の場合の条件を追加

--- cookpad/index.js
+++ cookpad/index.js
@@ -6,7 +6,7 @@ module.exports = async function (context, req) {
     const RssContent = await rssParser.Load();
     context.log(RssContent);
     RssContent.map(item =>{
-        if(!item.Thumbnail){
+        if(!item.Thumbnail || item.Thumbnail == null){
             item.Thumbnail = "https://axxis.co.jp/magazine/wp-content/uploads/2016/01/cookpad.jpg";
         }
         return item
