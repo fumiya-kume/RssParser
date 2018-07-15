fccfee33876c7484b1fdb28b6a406f88fe047a93
Fumiya Kume <fumiya.kume@hotmail.com>
2018-07-15 16:52:54 +0900

    余計な空白等を削除

--- cookpad/index.js
+++ cookpad/index.js
@@ -1,12 +1,9 @@
 const RssParser = require('../lib/RssParser');
 
-module.exports = async function(context, req) {
+module.exports = async function (context, req) {
     const pixivUrl = `https://techlife.cookpad.com/feed`;
     const rssParser = new RssParser(pixivUrl);
     const RssContent = await rssParser.Load("https://www.tumutoku.com/wp/wp-content/uploads/2017/09/rogo-297x300.jpg");
-
-    //context.log(RssContent);
     context.bindings.RssBlob = RssContent;
-
     context.res = { body: RssContent };
 };
