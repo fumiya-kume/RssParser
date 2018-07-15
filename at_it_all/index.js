ed04c9678c23e8c69c38e1b9e83850f9dd58b9d4
Fumiya Kume <fumiya.kume@hotmail.com>
2018-07-15 16:55:25 +0900

    Add @it Image URL

--- at_it_all/index.js
+++ at_it_all/index.js
@@ -3,7 +3,8 @@ const RssParser = require('../lib/RssParser');
 module.exports = async function (context, req) {
     const RssUrl = `https://rss.itmedia.co.jp/rss/2.0/ait.xml`;
     const rssParser = new RssParser(RssUrl);
-    const RssContent = await rssParser.Load();
+    const ThumbnailImageUrl = "http://image.itmedia.co.jp/images/logo/pcvheader_ait.png";
+    const RssContent = await rssParser.Load(ThumbnailImageUrl);
     context.log(RssContent);
     context.bindings.RssBlob = RssContent
     context.res = {body:RssContent};
