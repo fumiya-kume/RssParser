89bc7c1a65d70cca89932d797543d45cde49068e
Fumiya Kume <fumiya.kume@hotmail.com>
2018-07-15 10:22:36 +0900

    サムネイルを取得するようにした

--- Line/index.js
+++ Line/index.js
@@ -3,7 +3,8 @@ const RssParser = require('../lib/RssParser');
 module.exports = async function (context, req) {
     const RssUrl = `https://engineering.linecorp.com/ja/blog/rss2`;
     const rssParser = new RssParser(RssUrl);
-    const RssContent = await rssParser.Load();
+    const ThumbnailImageUrl = "https://lh3.googleusercontent.com/BkvRJsjYiEjb0-XKuop2AurqFKLhhu_iIP06TrCTGAq180P9Briv8Avz8ncLp7bOmCs";
+    const RssContent = await rssParser.Load(ThumbnailImageUrl);
     context.log(RssContent);
     context.bindings.RssBlob = RssContent
     context.res = {body:RssContent};
--- cookpad/index.js
+++ cookpad/index.js
@@ -1,16 +1,12 @@
 const RssParser = require('../lib/RssParser');
 
-module.exports = async function (context, req) {
+module.exports = async function(context, req) {
     const pixivUrl = `https://techlife.cookpad.com/feed`;
     const rssParser = new RssParser(pixivUrl);
-    const RssContent = await rssParser.Load();
-    context.log(RssContent);
-    RssContent.map(item =>{
-        if(!item.Thumbnail || item.Thumbnail == null){
-            item.Thumbnail = "https://axxis.co.jp/magazine/wp-content/uploads/2016/01/cookpad.jpg";
-        }
-        return item
-    })
-    context.bindings.RssBlob = RssContent
-    context.res = {body:RssContent};
+    const RssContent = await rssParser.Load("https://www.tumutoku.com/wp/wp-content/uploads/2017/09/rogo-297x300.jpg");
+
+    //context.log(RssContent);
+    context.bindings.RssBlob = RssContent;
+
+    context.res = { body: RssContent };
 };
